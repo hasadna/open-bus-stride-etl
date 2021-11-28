a7c37654dd57b43d9fc07e3116b5842d25d279fd
Ori Hoch <ori@uumpa.com>
2021-11-28 16:47:54 +0200

    move conversion to timezone aware to lower level

--- open_bus_stride_etl/siri/add_ride_durations.py
+++ open_bus_stride_etl/siri/add_ride_durations.py
@@ -1,9 +1,7 @@
-import datetime
 from pprint import pprint
 from textwrap import dedent
 from collections import defaultdict
 
-import pytz
 from sqlalchemy.engine import ResultProxy, Row
 
 from open_bus_stride_db.db import session_decorator, Session
@@ -27,17 +25,20 @@ def get_first_last_row(session, siri_ride_id, order_by):
         siri_ride_id=siri_ride_id, order_by=order_by)
     )
     row: Row = result.one_or_none()
-    return row
+    return {
+        'id': int(row.id) if row.id else None,
+        'recorded_at_time': common.utc(row.recorded_at_time) if row.recorded_at_time else None
+    } if row else None
 
 
 def update_first_last_vehicle_locations(siri_ride, first_row, last_row, stats):
     is_updated = False
-    if first_row and first_row.id != siri_ride.first_vehicle_location_id:
-        siri_ride.first_vehicle_location_id = first_row.id
+    if first_row and first_row['id'] != siri_ride.first_vehicle_location_id:
+        siri_ride.first_vehicle_location_id = first_row['id']
         is_updated = True
         stats['num_rows_first_vehicle_location_updated'] += 1
-    if last_row and last_row.id != siri_ride.last_vehicle_location_id:
-        siri_ride.last_vehicle_location_id = last_row.id
+    if last_row and last_row['id'] != siri_ride.last_vehicle_location_id:
+        siri_ride.last_vehicle_location_id = last_row['id']
         is_updated = True
         stats['num_rows_last_vehicle_location_updated'] += 1
     if not siri_ride.updated_first_last_vehicle_locations:
@@ -51,11 +52,11 @@ def update_duration_minutes(siri_ride, first_row, last_row, stats):
     is_updated = False
     if (
             first_row and last_row
-            and first_row.recorded_at_time
-            and last_row.recorded_at_time
-            and common.utc(first_row.recorded_at_time) < common.utc(last_row.recorded_at_time) < common.now_minus(hours=6)
+            and first_row['recorded_at_time']
+            and last_row['recorded_at_time']
+            and first_row['recorded_at_time'] < last_row['recorded_at_time'] < common.now_minus(hours=6)
     ):
-        siri_ride.duration_minutes = round((last_row.recorded_at_time - first_row.recorded_at_time).total_seconds() / 60)
+        siri_ride.duration_minutes = round((last_row['recorded_at_time'] - first_row['recorded_at_time']).total_seconds() / 60)
         stats['num_rows_updated_duration_minutes'] += 1
         is_updated = True
     elif siri_ride.updated_first_last_vehicle_locations < common.now_minus(days=2):
