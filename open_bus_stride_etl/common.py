ccb9885509a336f40b534bac1470ed407d02b92d
Ori Hoch <ori@uumpa.com>
2022-02-03 09:44:12 +0200

    fix handling of None value for airflow parameter value bug

--- open_bus_stride_etl/common.py
+++ open_bus_stride_etl/common.py
@@ -21,13 +21,19 @@ def utc(dt: datetime.datetime):
     return dt.astimezone(pytz.UTC)
 
 
+def is_None(val):
+    # due to a problem with airflow dag initialization, in some cases we get
+    # the actual string 'None' which we need to handle as None
+    return val is None or val == 'None'
+
+
 def parse_date_str(date, num_days=None):
     """Parses a date string in format %Y-%m-%d with default of today if empty
     if num_days is not None - will use a default of today minus given num_days
     """
     if isinstance(date, datetime.date):
         return date
-    elif not date or date is 'None':
+    elif not date or is_None(date):
         return datetime.date.today() if num_days is None else datetime.date.today() - datetime.timedelta(days=int(num_days))
     else:
         return datetime.datetime.strptime(date, '%Y-%m-%d').date()
