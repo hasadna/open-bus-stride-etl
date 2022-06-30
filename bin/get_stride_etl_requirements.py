ec7c8b73571f25801f40c42981f98e5e79fddfc2
Ori Hoch <ori@uumpa.com>
2022-06-30 11:29:48 +0300

    ci fix

--- bin/get_stride_etl_requirements.py
+++ bin/get_stride_etl_requirements.py
@@ -8,7 +8,7 @@ def main(stride_etl_commit):
     with open('stride-api-latest-commit.txt', 'r') as f:
         stride_api_latest_commit = f.read().strip()
     print(f'-r https://github.com/hasadna/open-bus-stride-api/raw/{stride_api_latest_commit}/requirements.txt')
-    print(f'https://github.com/hasadna/open-bus-stride-etl/archive/{stride_api_latest_commit}.zip')
+    print(f'https://github.com/hasadna/open-bus-stride-api/archive/{stride_api_latest_commit}.zip')
     with open('stride-client-latest-tag.txt', 'r') as f:
         stride_client_latest_tag = f.read().strip()
     print(f'open-bus-stride-client[all]=={stride_client_latest_tag}')
