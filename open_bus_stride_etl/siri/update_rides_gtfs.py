6460dc770a2ed56159c2c8bee02c97b5bcad64aa
Shay <shay@bionicstork.com>
2023-06-12 20:07:36 +0300

    Refresh view

--- open_bus_stride_etl/siri/update_rides_gtfs.py
+++ open_bus_stride_etl/siri/update_rides_gtfs.py
@@ -131,6 +131,10 @@ def main(min_date, max_date, num_days):
     with db.get_session() as session:
         session.execute("refresh materialized view gtfs_rides_agg")
         session.commit()
+    print("Refreshing gtfs_rides_agg_by_hour materialized view")
+    with db.get_session() as session:
+        session.execute("refresh materialized view gtfs_rides_agg_by_hour")
+        session.commit()
 
 def get_tommorow_date(date: str) -> str:
     date = datetime.datetime.strptime(date, GTFS_ROTE_DATE_FORMAT)
