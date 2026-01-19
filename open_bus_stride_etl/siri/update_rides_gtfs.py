2004f852e7aeac82d681a1249563b7f008ca28a4
Tamir K. <121674805+Tamir-K@users.noreply.github.com>
2026-01-19 23:01:23 +0200

    Refactor date handling in update_rides_gtfs.py

--- open_bus_stride_etl/siri/update_rides_gtfs.py
+++ open_bus_stride_etl/siri/update_rides_gtfs.py
@@ -1,15 +1,7 @@
-from datetime import date, timedelta
+import datetime
 from textwrap import dedent
 
 from open_bus_stride_db import db
-
-
-
-def parse_date_str(date):
-    """Parses a date string in ISO format (YYYY-MM-DD)."""
-    if isinstance(date, datetime.date):
-        return date
-    return datetime.date.fromisoformat(date)
 
 
 UPDATE_ROUTE_GTFS_RIDE_QUERY = """
@@ -19,17 +11,23 @@ FROM siri_route AS sr
 JOIN gtfs_route AS gr ON sr.line_ref = gr.line_ref
 JOIN gtfs_ride AS grd ON gr.id = grd.gtfs_route_id
 WHERE srd.scheduled_start_time = grd.start_time
-AND srd.scheduled_start_time BETWEEN :input_date AND :input_date + INTERVAL '1 day';
+AND srd.scheduled_start_time BETWEEN :date AND :date + INTERVAL '1 day';
 """
 
 
-def main(input_date=None):
-    input_date = date.today() if not input_date else parse_date_str(input_date)
-    print(f"{input_date=}")
+def parse_date(date):
+    """Parses a date string in ISO format (YYYY-MM-DD)."""
+    if isinstance(date, datetime.date):
+        return date
+    return datetime.date.fromisoformat(date)
+
+def main(date=None):
+    date = datetime.date.today() if not date else parse_date(date)
+    print(f"{date=}")
 
     with db.get_session() as session:
         sql = dedent(UPDATE_ROUTE_GTFS_RIDE_QUERY)
-        params = {"input_date": input_date}
+        params = {"date": date}
         res = session.execute(sql, params)
         updated_route_gtfs_ride_ids = res.rowcount
         print(f"{updated_route_gtfs_ride_ids=}")
